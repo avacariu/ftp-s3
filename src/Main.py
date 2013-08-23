da94f33573b5cc83ac3e36bfc1e2ee72aad3ed2b
Andrei Vacariu <vacariu.v@gmail.com>
2013-08-22 23:13:49 -0700

    Added a port number as an optional parameter.

--- src/Main.py
+++ src/Main.py
@@ -5,8 +5,19 @@ from S3FTPHandler import S3FTPHandler
 from pyftpdlib.servers import FTPServer
 
 import urllib
+import sys
 
 def main():
+    port = 21
+
+    # get an alternative port number
+    if len(sys.argv) == 2:
+        try:
+            port = int(sys.argv[1])
+        except ValueError:
+            print "Invalid port number specified."
+            sys.exit(1)
+
     authorizer = S3Authorizer()
 
     handler = S3FTPHandler
@@ -16,7 +27,7 @@ def main():
     handler.permit_foreign_addresses = True
     handler.passive_ports = range(60000, 65535)
 
-    server = FTPServer(('', 2121), handler)
+    server = FTPServer(('', port), handler)
     server.serve_forever()
 
 if __name__ == "__main__":
